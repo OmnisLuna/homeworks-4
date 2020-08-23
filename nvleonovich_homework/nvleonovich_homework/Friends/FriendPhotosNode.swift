061c48ab88da5a86638a60028bc63c63582170d4
Natalya Leonovich <nleonovich@avito.ru>
2020-08-24 00:22:55 +0300

    fix

--- nvleonovich_homework/nvleonovich_homework/Friends/FriendPhotosNode.swift
+++ nvleonovich_homework/nvleonovich_homework/Friends/FriendPhotosNode.swift
@@ -13,22 +13,13 @@ class FriendPhotosNode: ASCellNode {
     private func setupSubNodes() {
         DispatchQueue.main.async {
             self.imageNode.url = URL(string: self.photo.sizes[3].url)!
-            self.imageNode.contentMode = .scaleAspectFill
             self.addSubnode(self.imageNode)
             }
     }
 
     override func layoutSpecThatFits(_ constrainedSize: ASSizeRange) -> ASLayoutSpec {
-        var imageRatio: CGFloat = 0.5
-        if imageNode.image != nil {
-          imageRatio = (imageNode.image?.size.height)! / (imageNode.image?.size.width)!
-        }
-        let imagePlace = ASRatioLayoutSpec(ratio: imageRatio, child: imageNode)
-        let stackLayout = ASStackLayoutSpec.horizontal()
-        stackLayout.justifyContent = .start
-        stackLayout.alignItems = .start
-        stackLayout.style.flexShrink = 1.0
-        stackLayout.children = [imagePlace]
-        return ASInsetLayoutSpec(insets: UIEdgeInsets.zero, child: stackLayout)
+        let width = constrainedSize.max.width
+        imageNode.style.preferredSize = CGSize(width: width, height: width*photo.sizes[3].aspectRatio)
+        return ASWrapperLayoutSpec(layoutElement: imageNode)
     }
 }
